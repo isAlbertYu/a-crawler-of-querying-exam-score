9d37d06bc7879db04b03a6ea48236dde7d0e8858
isAlbertYu <34757591+isAlbertYu@users.noreply.github.com>
2018-04-10 15:03:55 +0800

    Update LoginConst.java

--- LoginConst.java
+++ LoginConst.java
@@ -1,27 +1,24 @@
 package com.albert.httpclient;
 
 import java.net.URI;
-
+//è¯¥æ–‡ä»¶ä¸­å­˜æ”¾å­¦å·ï¼Œå¯†ç ä¸è¯·æ±‚ç½‘ç«™ï¼Œå­¦å·ä¸å¯†ç éœ€RSAåŠ å¯†
 public final class LoginConst {
-	//Ñ§ºÅ
+	//å­¦å·
 	public static String username = 
-			"31f1e833ec62bf1a4989ec90f2bb1f2ce5889e60f399914edc62058d0d656655927ffbd35821173639ba417abeb124ac7a9a98500816482c69ba9968d43b7f07d36f9ef739429a9c808a2ce30bdb9e6deb15dc7e429051a0b981b1d1e168c003cbc88186ea836849a4b540ea4911813517af07d1f70c0c33e60a41e4d641253d";
+			"******************************************";
 		
-	//ÃÜÂë
+	//å¯†ç 
 	public static String password = 
-			"1f3bfd8057465b17bef399a40d52251e29461f0028f551e51e24a091e074dc7eaaab09dd1bc3f624c301fa1f1fd36cb152542c0fd0f34096787237aa3aa72ec8a03b01660ee36f58d195a4d85d369b51d1393dc0d5e4cfe0d9ead778033fc486cb3462556eba9c6719f3fe9e78e0707043d2e074e13d3cd74ce0879f7cddcfe5";
+			""******************************************";";
 	
-	//µÇÂ¼Ò³ÃæµÄÁ´½Ó
+	//ç™»å½•é¡µé¢çš„é“¾æ¥
 	public static String loginURLString = 
-			"https://pass.hust.edu.cn/cas/login?service=http%3A%2F%2Fyjs.hust.edu.cn%2Fssfw%2Flogin_cas.jsp";
-	
-	//public static URI loginURL = new URI("https://pass.hust.edu.cn/cas/login?service=http%3A%2F%2Fyjs.hust.edu.cn%2Fssfw%2Flogin_cas.jsp");
-	
-	//³É¼¨²éÑ¯Ò³Ãæ
+			"https://pass.hust.edu.cn/cas/login?service=http%3A%2F%2Fyjs.hust.edu.cn%2Fssfw%2Flogin_cas.jsp";	
+	//æˆç»©æŸ¥è¯¢é¡µé¢
 	public static String gradeURLString = 
 			"http://yjs.hust.edu.cn/ssfw/pygl/cjgl/cjcx.do";
 	
-	//ÍøÒ×ÔÆÒ³Ãæ
+	//ç½‘æ˜“äº‘é¡µé¢
 	public static String wyyURIString = 
 			"http://music.163.com/#/song/439915614/?userid=79496962";
 	
